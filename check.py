31718c7501a8bfd61f69698ed66b133c963d2f6a
Daniel Porteous <danielporteous1@gmail.com>
2021-04-14 13:59:54 -0700

    Read sites from sites.comma instead of vars file

--- check.py
+++ check.py
@@ -2,11 +2,12 @@ import asyncio
 import argparse
 import functools
 import json
+import os 
 import sys
 import urllib.request
 
 
-VARS_FILE = "vars.json"
+SITES_COMMA_FILE = "sites.comma"
 
 
 async def check_site(url, debug=False):
@@ -22,34 +23,41 @@ async def check_site(url, debug=False):
     except urllib.error.HTTPError as e:
         code = e.code
         if debug:
-            print("HTTPError:", e)
+            print("HTTPError:", url, e)
     except urllib.error.URLError as e:
         # Timeout
         code = 999
         if debug:
-            print("URLError (timeout):", e)
+            print("URLError (timeout):", url, e)
     return url, code
 
 
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--urls", nargs="*")
-    parser.add_argument("--read-from-vars", action="store_true")
-    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--read-from-sites-comma", action="store_true")
+    parser.add_argument("--debug", "-d", action="store_true")
     args = parser.parse_args()
 
-    if not args.urls and not args.read_from_vars:
-        print("Please pass either --urls or --read-from-vars")
+    if not args.urls and not args.read_from_sites_comma:
+        print("Please pass either --urls or --read-from-sites-comma")
         sys.exit(1)
 
-    if args.read_from_vars:
-        with open(VARS_FILE, "r") as f:
-            d = json.load(f)
-            args.urls = [d["domains"]]
+    if args.read_from_sites_comma:
+        try:
+            with open(SITES_COMMA_FILE, "r") as f:
+                args.urls = [s.rstrip() for s in f.read().split(",")]
+        except FileNotFoundError:
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            path = os.path.join(dir_path, SITES_COMMA_FILE)
+            with open(path, "r") as f:
+                args.urls = [s.rstrip() for s in f.read().split(",")]
 
     urls = set()
     for u in args.urls:
         urls |= set(u.split(","))
+
+    print(urls)
 
     tasks = [check_site(url, debug=args.debug) for url in urls]
     ret = await asyncio.gather(*tasks)
